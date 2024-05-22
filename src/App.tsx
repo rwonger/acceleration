7c982e2aedf99ae8fd4015c4a1bbb7f30855f622
Ricky <rwong22@student.ubc.ca>
2024-05-22 00:01:57 -0700

    fixed overflow bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import './App.css'
 import Clock from './Clock'
-import bg from '/public/background.jpg'
+import GreetingMessage from './GreetingMessage'
 
 function App() {
 
   return (
-  
-      // <div className='container' style={{ backgroundImage: `url(${bg})` }}>
-        <div className='main-content font-semibold text-slate-100'>
-          <div className='main-clock'>
-            <Clock />
-          </div>
-          <h2 className='greeting'>Good afternoon, Ricky</h2>
-        </div>
-      // </div>
-    
+    <div className='main-content font-semibold text-slate-100'>
+      <div className='main-clock mb-1'>
+        <Clock />
+      </div>
+      <GreetingMessage />
+    </div>
   )
 }
 
---
+++ src/GreetingMessage.tsx
@@ -0,0 +1,13 @@
+
+
+function GreetingMessage() {
+
+
+
+    return (
+        <div>
+            <h2 className='greeting'>Good afternoon, Ricky</h2>
+        </div>
+    )
+}
+export default GreetingMessage;
