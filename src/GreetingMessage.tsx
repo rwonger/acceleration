1ad8da548fe4e0df8757a4e9ffdaaf5ae90e6fea
Ricky <rwong22@student.ubc.ca>
2024-05-22 01:57:33 -0700

    added name editing functionality

--- src/GreetingMessage.tsx
+++ src/GreetingMessage.tsx
@@ -1,21 +1,17 @@
-import { useState } from "react";
-
+import React, { useState } from "react";
 
 function GreetingMessage() {
     const [time, setTime] = useState(getCurrentTime());
+    const [name, setName] = useState('Ricky');
+    const [isEditing, setIsEditing] = useState(false);
 
     function getCurrentTime() {
         const date = new Date();
         const hours = date.getHours();
-
-        return { hours }
+        return { hours };
     }
 
     function updateMOTD(curr: number) {
-        // morning is defined by 00:00 to 11:59
-        // afternoon is defined by 12:00 to 17:59
-        // evening is defined by 18:00 to 23:59
-        
         if (0 <= curr && curr < 12) {
             return "morning";
         } else if (12 <= curr && curr < 18) {
@@ -25,10 +21,49 @@ function GreetingMessage() {
         }
     }
 
+    function handleNameDoubleClick() {
+        setIsEditing(true);
+    }
+
+    function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setName(e.target.value);
+    }
+
+    function handleNameBlur() {
+        setIsEditing(false);
+    }
+
+    function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            setIsEditing(false); // Exit editing mode
+        }
+    }
+
+    function handleNameRender() {
+        return (
+            isEditing ? (
+                <input
+                    className="nameInput"
+                    type="text"
+                    value={name}
+                    onChange={handleNameChange}
+                    onBlur={handleNameBlur}
+                    onKeyDown={handleKeyPress}
+                    autoFocus 
+                />
+            ) : (
+                <span>{name}</span>
+            )
+        )
+    }
+
     return (
         <div>
-            <h2 className='greeting'>Good {updateMOTD(time.hours)}, Ricky</h2>
+            <h2 className='greeting' onDoubleClick={handleNameDoubleClick}>
+                Good {updateMOTD(time.hours)}, {handleNameRender()}.
+            </h2>
         </div>
-    )
+    );
 }
+
 export default GreetingMessage;
