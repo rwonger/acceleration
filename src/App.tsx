acfb761b465f4c8b38f4b64f542c1d19d2aa6b86
Ricky <rwong22@student.ubc.ca>
2024-06-06 20:53:59 -0700

    fixed buggy bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,28 @@ import MainContent from './MainContent';
 import Fade from './anim/Fade';
 
 function App() {
-  localStorage.clear();
+  // localStorage.clear();
 
   const [newUser, setNewUser] = useState(!localStorage.getItem("userName"));
   const [username, setUsername] = useState(() => localStorage.getItem('userName') || "");
   const [showMainContent, setShowMainContent] = useState(false);
+  const [buffer, setBuffer] = useState(0);
 
   useEffect(() => {
     if (!newUser) {
       localStorage.setItem('userName', username);
       const timer = setTimeout(() => {
         setShowMainContent(true);
-      }, 1000); //timeout duration in ms
+      }, buffer); //timeout duration in ms
 
       return () => clearTimeout(timer);
     }
-  }, [newUser, username]);
+  }, [buffer, newUser, username]);
 
   return (
     <div className='text-slate-100'>
       <Fade in={newUser}>
-        <Startup onComplete={() => setNewUser(false)} setUsername={setUsername} username={username} />
+        <Startup onComplete={() => {setNewUser(false); setBuffer(1000)}} setUsername={setUsername} username={username} />
       </Fade>
       <Fade in={!newUser && showMainContent}>
           <MainContent username={username} />
